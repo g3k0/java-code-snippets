d8d70315f74afa08c9f6a38981ea8e9b42485304
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-10 11:55:06 +0200

    strings

---
+++ src/ApplicationString.java
@@ -0,0 +1,15 @@
+public class ApplicationString {
+
+    public static void main(String[] args) {
+
+        String text = "hello";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+
+        System.out.println(text);
+        System.out.println(greeting);
+
+        System.out.println("hello" + " " + "Bob");
+    }
+}
