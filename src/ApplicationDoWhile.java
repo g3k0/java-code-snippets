b2a60a1c9b141fdda7dca7a15a89344b799337e0
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-11 09:57:34 +0200

    do while loop

---
+++ src/ApplicationDoWhile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ApplicationDoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        do {
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        while(value != 5);
+        System.out.println("Got 5!");
+    }
+}
