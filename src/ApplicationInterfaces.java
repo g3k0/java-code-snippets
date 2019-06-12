5e92d017c8a821d349730c133f6bbbfcaed4f351
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-12 10:58:57 +0200

    interfaces

---
+++ src/ApplicationInterfaces.java
@@ -0,0 +1,55 @@
+class Machinet implements Info {
+    private int id = 7;
+
+    public void start() {
+        System.out.println("Machinet started");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Machinet id is: " + id);
+    }
+}
+
+class Persont implements Info {
+    private String name;
+
+    public Persont(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hello there");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Persont name is: " + name);
+    }
+}
+
+public class ApplicationInterfaces {
+    public static void main(String[] args) {
+        Machinet mach1 = new Machinet();
+        mach1.start();
+
+        Persont person1 = new Persont("Teo");
+        person1.greet();
+
+        // I can use interface as type:
+        Info info1 = new Machinet();
+        info1.showInfo();
+
+
+        // I can point to object that implements the interface:
+        Info info2 = person1;
+        info2.showInfo();
+
+        outputInfo(person1);
+    }
+
+    // I can pass info in method as paarameter
+    private static void outputInfo(Info info) {
+        info.showInfo();
+    }
+}
