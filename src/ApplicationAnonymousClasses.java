a9f8ac87499a96149a2579619bf6a88fc62f19ed
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-13 10:27:27 +0200

    anonymous classes

---
+++ src/ApplicationAnonymousClasses.java
@@ -0,0 +1,30 @@
+class MachineA {
+    public void start() {
+        System.out.println("Starting machine...");
+    }
+}
+
+interface PlantA {
+    public void grow();
+}
+
+public class ApplicationAnonymousClasses {
+    public static void main(String[] args) {
+
+        // this is a child class of machineA without name
+        MachineA machine1 = new MachineA() {
+            @Override public void start() {
+                System.out.println("Camera starting...");
+            }
+        };
+        machine1.start();
+
+        // implementing interface with anonymous class.
+        PlantA plant1 = new PlantA() {
+            public void grow() {
+                System.out.println("Plant growing");
+            }
+        };
+        plant1.grow();
+    }
+}
