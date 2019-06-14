15c67181c7f39d683c4b532479d93731e5b53f55
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-14 12:27:51 +0200

    array list

---
+++ src/ApplicationArrayList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ApplicationArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        // add
+        numbers.add(10);
+        numbers.add(100);
+        numbers.add(40);
+
+        // retrieve
+        System.out.println(numbers.get(0));
+
+        // loop
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+
+        // remove
+        numbers.remove(numbers.size() -1);
+    }
+}
