d70c91365019e5193e1742a20f871f9a92227e96
Christan Palazzo <christian.palazzo@kaleyra.com>
2019-06-26 13:45:52 +0200

    removed package

--- src/Packages.java
+++
@@ -1,9 +0,0 @@
-import ocean.Fish;
-import ocean.Seaweed;
-
-public class Packages {
-    public static void main(String[] args) {
-        Fish fish = new Fish();
-        Seaweed weed = new Seaweed();
-    }
-}
